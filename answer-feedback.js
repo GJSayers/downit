45fd7e721e2ec78537613ed9343ad7e6672f046a
GJSayers <gemma.sayers1@btinternet.com>
2021-03-06 13:08:08 +0000

    add edits to answer-feedback.js add / remove class

--- answer-feedback.js
+++ answer-feedback.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
 
-    const button = document.querySelector('input');
 
-    button.addEventListener('click',answerFeeback);
+    document.getElementById("button").addEventListener('click',answerFeeback);
 
     let answerFeedback {
         if (question.option === answer.option) {
 
             $("button").removeClass("button").addClass("button-correct");
-
-
             
         } else {
 
