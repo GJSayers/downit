3789497306d79db08ea6744ef405133f15d088b7
Sean Young <seanyoung247@gmail.com>
2021-03-06 17:38:42 +0000

    Feat: Adds javascript function to submit a form via ajax

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,24 @@ $( document ).ready(function () {});
 $( "#quiz-player-form .player-name" ).click(function() {
   $( this ).val('');
 });
+
+/*
+ * General function for submitting forms via AJAX
+ */
+function submitFormAJAX(event, callbackSuccess) {
+  // Get form data
+  var data = new FormData(event.target);
+  var serialised = {};
+  // serialise it into key/value pairs that can be converted to JSON
+  for (var key of data.keys()) {
+    serialised[key] = data.get(key);
+  }
+  // Make AJAX request
+  $.ajax({
+    type : "POST",
+    url : $(event.target).prop("data-ajax"), // Get route from form attribute
+    contentType : 'application/json;charset=UTF-8',
+    data : JSON.stringify(serialised),
+    success : callbackSuccess
+  });
+}
