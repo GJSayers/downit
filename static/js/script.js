4a87f00d46fcd3a818afd1370e8a0a68686b87ff
Sean Young <seanyoung247@gmail.com>
2021-03-06 19:07:26 +0000

    Feat: Adds frontend code for ajax answer dispatch

--- static/js/script.js
+++ static/js/script.js
@@ -5,12 +5,24 @@ $( "#quiz-player-form .player-name" ).click(function() {
   $( this ).val('');
 });
 
+$( "input[name='answer']" ).change(function(event) {
+  console.log( `Selected option: ${$( this ).val()}` );
+  //Player has selected so disable the other options
+  $( "input[name='answer']:not(:checked)" ).prop('disabled', true);
+  //Submit the answer to the server
+  submitFormAJAX($( "#quiz-form" )[0], checkAnswerCallback);
+});
+
+function checkAnswerCallback(responce) {
+  console.log(responce);
+}
+
 /*
  * General function for submitting forms via AJAX
  */
-function submitFormAJAX(event, callbackSuccess) {
+function submitFormAJAX(form, callbackSuccess) {
   // Get form data
-  var data = new FormData(event.target);
+  var data = new FormData(form);
   var serialised = {};
   // serialise it into key/value pairs that can be converted to JSON
   for (var key of data.keys()) {
@@ -19,7 +31,7 @@ function submitFormAJAX(event, callbackSuccess) {
   // Make AJAX request
   $.ajax({
     type : "POST",
-    url : $(event.target).prop("data-ajax"), // Get route from form attribute
+    url : $( form ).attr("data-ajax"), // Get route from form attribute
     contentType : 'application/json;charset=UTF-8',
     data : JSON.stringify(serialised),
     success : callbackSuccess
