4deb58e327792fdb48f153b7c5fd61c7da22a23a
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-03-06 22:06:39 +0000

    Changes styling to use Modulus number.

--- pint.js
+++ pint.js
@@ -6,29 +6,30 @@ $("button").click(function(){
 
     let scoreAsNumber = parseInt(score);
 
+    let scoreModulus = scoreAsNumber % 5;
+
+    console.log(scoreModulus);
+
     const firstGulp = [1, 6, 11, 16, 21, 26, 31, 36, 41];
     const secondGulp = [2, 7, 12, 17, 22, 27, 32, 37, 42];
     const thirdGulp = [3, 8, 13, 18, 23, 28, 33, 38, 43];
     const forthGulp = [4, 9, 14, 19, 24, 29, 34, 39, 44];
     const fifthGulp = [5, 10, 15, 20, 25, 30, 35, 40, 45];
 
-
-    console.log(score);
-
     function refill() {
         $("div").removeClass("pint5").addClass("pint0");
         $("div").effect( "slide", "fast" );
     }
 
-    if (firstGulp.includes(scoreAsNumber)) {
+    if (scoreModulus === 1) {
         $("div").effect( "bounce", "slow" ).removeClass("pint0").addClass("pint1",);
-    } else if (secondGulp.includes(scoreAsNumber)) {
+    } else if (scoreModulus === 2) {
         $("div").effect( "bounce", "slow" ).removeClass("pint1").addClass("pint2");
-    } else if (thirdGulp.includes(scoreAsNumber)) {
+    } else if (scoreModulus === 3) {
         $("div").effect( "bounce", "slow" ).removeClass("pint2").addClass("pint3");
-    } else if (forthGulp.includes(scoreAsNumber)) {
+    } else if (scoreModulus === 4) {
         $("div").effect( "bounce", "slow" ).removeClass("pint3").addClass("pint4");
-    } else if (fifthGulp.includes(scoreAsNumber)) {
+    } else if (scoreModulus === 0) {
         $("div").effect( "bounce", "slow" ).removeClass("pint4").addClass("pint5");
         $("div").effect( "drop", 'fast' );
         setTimeout(refill, 1000);
