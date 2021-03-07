b48e6d1b9cbb67faac5f13ad90d347f85ca22913
GJSayers <gemma.sayers1@btinternet.com>
2021-03-07 18:56:37 +0000

    edit js to jquery on answer-feedback.js

--- answer-feedback.js
+++ answer-feedback.js
@@ -1,49 +1,12 @@
 $(document).ready(function () {
+   $(".quiz-option").each(function() {
+       
+       for (let i=0; i< quiz-options.length; i++); {
 
-    const quizOption = document.getElementsByClassName(".quiz-option");
-    const options = Array.from(document.getElementsByClassName(".options"));
-
-    let answerValue = document.getElementsById("#correctAnswer.value");
-
-    let correctAnswer = (answerValue - quizOption.value === 0);
-
-$(".quiz-option.options").addEventListner("click",answerFeedback);
-
-    let answerFeedback 
-    function answerFeedback(){
-        quizOption.options.forEach(); {
-
-            if (quizOption.options === correctAnswer) {
-
-                $("quiz-option").addClass("correct");
-  
-            } else {
-                
-                 $("quiz-option").addClass("wrong");
-            }
-            // const number = options.dataset["number"];
-
-        }
-    }
-});
-
-
-   // function()
-
-   // .quiz-form.forEach(.quiz-option => {
- //.quiz-option.addEventListner("click",)
-        
-  //  });
-
-   // document.getElementById("button").addEventListener('click',answerFeeback);
-
-  //  let answerFeedback function(){
-  //      if (question.option === answer.option) {
-
-   //         $("button").removeClass("button").addClass("button-correct");
-            
-   //     } else {
-
-   //         $("button").removeClass("button").addClass("button-incorrect");
-            
-    
+            if ($("quizOption").val() ===  $("responce").correct_answer) 
+             $("quiz-option").addClass("correct");
+              else
+                 $("quiz-option").addClass("wrong");     
+      };
+   });
+})
